85f2fe92cb157a7dccbf423f358d0e3c6623018f
molingran <moreant@foxmail.com>
2021-05-03 15:32:54 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <p>
+    <a
+      href="https://vitejs.dev/guide/features.html"
+      target="_blank"
+    >
+      Vite Documentation
+    </a>
+    |
+    <a
+      href="https://v3.vuejs.org/"
+      target="_blank"
+    >Vue 3 Documentation</a>
+  </p>
+
+  <Button
+    type="primary"
+    @click="state.count++"
+  >count is: {{ state.count }}</Button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+  <p class="text-9xl shadow-2xl">Tailwind </p>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+import { Button } from 'vant';
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
